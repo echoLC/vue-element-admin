e4c5f684168b216ca8c318be99a3e45e3f270e91
echoLC <651644100@qq.com>
2018-04-14 11:16:20 +0800

    添加了公共样式和normalize

--- src/main.js
+++ src/main.js
@@ -1,18 +1,24 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import ElementUI from 'element-ui'
 import App from './App'
 import router from './router'
 import store from './store'
 import * as filters from './filter'
 
-import './styles/common.css'
+// import styles
+import './styles/index.scss'
+import 'normalize.css/normalize.css'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
 Object.keys(filters).forEach((key) => {
   Vue.filter(key, filters[key])
 })
+
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
